7498742e1cddfa9604d09227581c348cbc43bcb4
callme-asyer <53029091+callme-asyer@users.noreply.github.com>
2019-09-13 09:24:55 +0700

    tugas2.js-tugas niomic

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var umur=22;
+var nama="asyer";
+var menikah=false;
+var kosong=null;
+var tidakAda;
+var kelompok=[1,2,3,4,5];
+
+console.log(umur+ " \n" + nama + " \n" + menikah + " \n" + kosong + " \n" + tidakAda + " \n" + kelompok);
